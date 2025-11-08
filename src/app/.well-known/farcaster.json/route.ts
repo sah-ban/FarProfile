aaa4b62f6163b86fd9fa8891dd9c6fe58c9cf598
sahban <sahbaan@proton.me>
2025-11-08 18:46:12 +0530

    update manifest

--- src/app/.well-known/farcaster.json/route.ts
+++ src/app/.well-known/farcaster.json/route.ts
@@ -5,9 +5,9 @@ export async function GET() {
     accountAssociation: {
       header:
         "eyJmaWQiOjI2ODQzOCwidHlwZSI6ImN1c3RvZHkiLCJrZXkiOiIweDIxODA4RUUzMjBlREY2NGMwMTlBNmJiMEY3RTRiRkIzZDYyRjA2RWMifQ",
-      payload: "eyJkb21haW4iOiJmYXItcHJvZmlsZS52ZXJjZWwuYXBwIn0",
+      payload: "eyJkb21haW4iOiJwcm9maWxlLml0c2Nhc2hsZXNzLmNvbSJ9",
       signature:
-        "MHg3NDQ4MTFlMDY5MDQ1NTAzYTIxNTQ2NTEwMGYxOGY3NmViZDI0NmRhMTJjZTM3M2I3NWJkNWVlNmFmNTRjNmE3MThjMDA1NTZlYTQ1OGQyNDBjNzJmNzE1NWY1N2ViNGRjZGM0NDY0Nzk1ODhmNzg1YzEyYzZiZDlkY2M2NGFhNDFi",
+        "7sAQEue8gL0l3PJRknyejkptB6v3Jo4Bk8uhZFFOaGhU+kOcdY4j3+wB+R9ot2y5nbgUIxcvyS5HozGsRPHxdRw=",
     },
     frame: {
       version: "1",
@@ -21,6 +21,17 @@ export async function GET() {
       webhookUrl: `${appUrl}/api/webhook`,
       castShareUrl: appUrl,
       primaryCategory: "social",
+      subtitle: "Farprofile - Farcaster Profile",
+      description:
+        "user's profile information.",
+      ogImageUrl: `${appUrl}/og.png`,
+      tags: ["farcaster", "profile", "account", "created", "farprofile"],
+      heroImageUrl: `${appUrl}/og.png`,
+      tagline: "Discover your Farcaster Profiles",
+      ogTitle: "FarProfile - Farcaster Profile",
+      ogDescription:
+        "user's profile information.",
+      requiredChains: ["eip155:8453"],
       baseBuilder: {
         allowedAddresses: ["0x06e5B0fd556e8dF43BC45f8343945Fb12C6C3E90"],
       },
