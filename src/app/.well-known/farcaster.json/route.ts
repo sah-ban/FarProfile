a2ee9dab088e10fd396e17bad3fecc2678666f3e
sahban <sahbaan@proton.me>
2026-09-19 21:54:48 +0530

    refactor: update GET function to accept request parameter and handle account associations

--- src/app/.well-known/farcaster.json/route.ts
+++ src/app/.well-known/farcaster.json/route.ts
@@ -1,14 +1,30 @@
-export async function GET() {
+export async function GET(request: Request) {
+  const domain = request.headers.get("host")?.split(":")[0] || "";
   const appUrl = process.env.NEXT_PUBLIC_URL;
 
+  const oldDomain = "profile.itscashless.com";
+
+  const oldAccountAssociation = {
+    header:
+      "eyJmaWQiOjI2ODQzOCwidHlwZSI6ImN1c3RvZHkiLCJrZXkiOiIweDIxODA4RUUzMjBlREY2NGMwMTlBNmJiMEY3RTRiRkIzZDYyRjA2RWMifQ",
+    payload: "eyJkb21haW4iOiJwcm9maWxlLml0c2Nhc2hsZXNzLmNvbSJ9",
+    signature:
+      "7sAQEue8gL0l3PJRknyejkptB6v3Jo4Bk8uhZFFOaGhU+kOcdY4j3+wB+R9ot2y5nbgUIxcvyS5HozGsRPHxdRw=",
+  };
+
+  const newAccountAssociation = {
+    header:
+      "eyJmaWQiOjI2ODQzOCwidHlwZSI6ImN1c3RvZHkiLCJrZXkiOiIweDIxODA4RUUzMjBlREY2NGMwMTlBNmJiMEY3RTRiRkIzZDYyRjA2RWMifQ",
+    payload: "eyJkb21haW4iOiJmYXJwcm9maWxlLnNhaGJhbi5kZXYifQ",
+    signature:
+      "lJTgFyPqBi4I7qILmLP7WJoTXm59fLTIwPBrrEOMpZ9gQxT7LHuEYD7MAaAykiXgw91WRR6y7o8lT2/zTyY3Uxs=",
+  };
+
+  const accountAssociation =
+    domain === oldDomain ? oldAccountAssociation : newAccountAssociation;
+
   const config = {
-    accountAssociation: {
-      header:
-        "eyJmaWQiOjI2ODQzOCwidHlwZSI6ImN1c3RvZHkiLCJrZXkiOiIweDIxODA4RUUzMjBlREY2NGMwMTlBNmJiMEY3RTRiRkIzZDYyRjA2RWMifQ",
-      payload: "eyJkb21haW4iOiJwcm9maWxlLml0c2Nhc2hsZXNzLmNvbSJ9",
-      signature:
-        "7sAQEue8gL0l3PJRknyejkptB6v3Jo4Bk8uhZFFOaGhU+kOcdY4j3+wB+R9ot2y5nbgUIxcvyS5HozGsRPHxdRw=",
-    },
+    accountAssociation,
     frame: {
       version: "1",
       name: "FarProfile",
